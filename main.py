8ecfcb9c2c2522ae6123b7607d65d0eb9147f87d
John Dunn <johnwdunn20@gmail.com>
2024-04-23 11:34:39 -0700

    query engine

---
+++ main.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+import os
+import pandas as pd
+from llama_index.query_engine import PandasQueryEngine
+
+load_dotenv()
+
+# Load the csv data
+population_path = os.path.join(os.path.dirname(__file__), 'data', 'population.csv')
+population_df = pd.read_csv(population_path)
+
+# create a query engine so that we can query the data
+population_query_engine = PandasQueryEngine(df=population_df)
+
