d91496855103d9a3557cf0e1718aff50cfc4c1cd
John Dunn <johnwdunn20@gmail.com>
2024-04-23 18:08:32 -0700

    importing prompts to main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 import pandas as pd
 from llama_index.query_engine import PandasQueryEngine
+from prompts import new_prompt, instruction_str
 
 load_dotenv()
 
