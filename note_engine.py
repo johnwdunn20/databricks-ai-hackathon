b172fe23258fb19e086866604a9c4132c81e9b16
John Dunn <johnwdunn20@gmail.com>
2024-04-29 15:06:08 -0700

    func to save notes

--- note_engine.py
+++ note_engine.py
@@ -1,4 +1,17 @@
 # FunctionTool is a wrapper around any function that converts it to a tool (supports both sync and async)
 from llama_index.core.tools import FunctionTool
+import os
+
+# create a file to save notes
+note_file = os.path.join(os.path.dirname(__file__), 'data', 'notes.txt')
 
 # function takes in a note and saves it to a file
+def save_note(note: str):
+    # create the file if it does not exist
+    if not os.path.exists(note_file):
+        open(note_file, 'w').close()
+    # append the note to the file
+    with open(note_file, 'a') as f:
+        f.writelines([note + '\n'])
+        
+    return "Note saved"
