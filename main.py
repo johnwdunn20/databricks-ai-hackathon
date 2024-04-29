53614cb63095bc86e23f908ea1eedd0beb5d6e93
John Dunn <johnwdunn20@gmail.com>
2024-04-29 14:28:20 -0700

    updated rag to query data

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 import os
 import pandas as pd
-from llama_index.core.query_engine import PandasQueryEngine
+from llama_index.experimental import PandasQueryEngine
 from prompts import new_prompt, instruction_str
 
 load_dotenv()
@@ -13,7 +13,7 @@ population_df = pd.read_csv(population_path)
 # create a query engine so that we can query the data
 # verbose=True will print out details from the query engine
 population_query_engine = PandasQueryEngine(df=population_df, verbose=True, instruction_str=instruction_str)
-population_query_engine.update_prompt({"pandas_prompt" :new_prompt})
+population_query_engine.update_prompts({"pandas_prompt": new_prompt})
 
 # Test the query engine
 population_query_engine.query("What is the population of Canada?")
