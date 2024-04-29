76a9bd6989fb54f27f0589fc85d8d490df3a6e59
John Dunn <johnwdunn20@gmail.com>
2024-04-29 15:02:53 -0700

    adding notes

---
+++ note_engine.py
@@ -0,0 +1,4 @@
+# FunctionTool is a wrapper around any function that converts it to a tool (supports both sync and async)
+from llama_index.core.tools import FunctionTool
+
+# function takes in a note and saves it to a file
