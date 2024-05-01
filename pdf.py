5d14bd2b14cd8698d44d2dd9de7d0e4370857085
John Dunn <johnwdunn20@gmail.com>
2024-04-30 19:45:36 -0700

    get index func

--- pdf.py
+++ pdf.py
@@ -10,7 +10,14 @@ def get_index(data, index_name):
     index = None
     # if index name doesn't exist, create it
     if not os.path.exists(index_name):
-        os.makedirs(index_name)
+        print(f"Creating index {index_name}")
+        # create the index
+        index = VectorStoreIndex.from_documents(data, show_progress=True)
+        # store the index
+        index.storage_context.persist(persist_dir=index_name)
+    # if it does exist, load it
+    else:
+        index = load_index_from_storage(StorageContext.from_defaults(persist_dir=index_name))
 
 # we can just create the index once and store it in the storage
 
