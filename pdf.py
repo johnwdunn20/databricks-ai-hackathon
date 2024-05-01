2f33ec34b9723ff0ae1fe6a5cdcd5406eed2521b
John Dunn <johnwdunn20@gmail.com>
2024-04-30 19:41:49 -0700

    pdf

--- pdf.py
+++ pdf.py
@@ -4,3 +4,15 @@
 
 import os
 from llama_index.core import VectorStoreIndex, StorageContext, load_index_from_storage
+from llama_index.readers import PDFReader
+
+def get_index(data, index_name):
+    index = None
+    # if index name doesn't exist, create it
+    if not os.path.exists(index_name):
+        os.makedirs(index_name)
+
+# we can just create the index once and store it in the storage
+
+pdf_path = os.path.join(os.path.dirname(__file__), 'data', 'United_States.pdf')
+US_pdf = PDFReader().load_data(file=pdf_path)
