37f3f8e53401b1acda9c58f7a15ecda3d11fc897
John Dunn <johnwdunn20@gmail.com>
2024-05-06 16:44:51 -0700

    sample starter tutorial for llama index

--- llama_index/starter.py
+++ llama_index/starter.py
@@ -1,2 +1,14 @@
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 
+# Might need to load dotenv if I don't export it in my shell
+
+# load data from the data directory
+documents = SimpleDirectoryReader('data').load_data()
+# create an index on that data
+index = VectorStoreIndex.from_documents(documents)
+# query engine
+query_engine = index.as_query_engine()
+
+# test the query engine
+response = query_engine.query('What did the author do growing up?')
+print(response)
